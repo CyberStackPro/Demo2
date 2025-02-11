330420a2c85bc0531b4ed38deb9957ea0e9bc0b0
CyberStackPro <yeabsragebriel@gmail.com>
2025-02-11 16:09:26 +0300

    Add Serialization

---
+++ store/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from store.views import product_detail
+from . import views
+
+
+urlpatterns = [
+    path('products/', views.product_list, name='product_list'),
+    path('products/<int:id>/', views.product_detail, name='product_detail'),
+
+    # path('', views.product_list, name='product_list'),
+    # path('tags/<slug:tag_slug>', views.product_list, name='product_list_by_tag'),
+    # path('<int:id>/<slug:slug>', views.product_detail, name='product_detail'),
+]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import rest_framework
+from .models import Product
+from .serializers import ProductSerializers
+
 
 # Create your views here.
+
+
+# def product_list(request):
+#     return render(request, "product_list.html")
+
+@api_view()
+def product_list(request):
+    queryset = Product.objects.all()
+    serializer = ProductSerializers(queryset, many=True)
+    return Response(serializer.data)
+
+
+@api_view()
+def product_detail(request, id):
+    product = get_list_or_404(Product, pk=id)
+    serializers = ProductSerializers(product)
+    return Response(serializers.data)
+    # try:
+    #     product = Product.objects.get(pk=id)
+    #     serializer = ProductSerializers(product)
+    #     return Response(serializer.data)
+    # except Product.DoesNotExist:
+    #     return Response(status=status.HTTP_404_NOT_FOUND)
