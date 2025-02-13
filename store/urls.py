4c2517cceb8d38eb29db5c09832e33981a9c0135
CyberStackPro <yeabsragebriel@gmail.com>
2025-02-13 16:48:37 +0300

    Add Routers

--- store/urls.py
+++ store/urls.py
@@ -1,15 +1,30 @@
-from django.urls import path
+from django.urls import path, include
+# import include
+from rest_framework.routers import SimpleRouter
+from pprint import pprint
+from playground.urls import urlpatterns
 from . import views
 
+router = SimpleRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
+pprint(
+    router.urls
+
+)
+
+# urlpatterns = [
+#     path('products/', views.product_list, name='product_list'),
+#     path('products/', views.ProductList.as_view(), name='product_list'),
+#     path('products/<int:pk>/', views.ProductDetail.as_view(), name='product_detail'),
+#     path('collections/', views.CollectionList.as_view(), name='collection_list'),
+#     path('collections/<int:pk>/', views.CollectionDetail.as_view(),
+#          name='collection-detail'),
+#     path('', views.product_list, name='product_list'),
+#     path('tags/<slug:tag_slug>', views.product_list, name='product_list_by_tag'),
+#     path('<int:id>/<slug:slug>', views.product_detail, name='product_detail'),
+# ]
 
 urlpatterns = [
-    # path('products/', views.product_list, name='product_list'),
-    path('products/', views.ProductList.as_view(), name='product_list'),
-    path('products/<int:pk>/', views.ProductDetail.as_view(), name='product_detail'),
-    path('collections/', views.CollectionList.as_view(), name='collection_list'),
-    path('collections/<int:pk>/', views.CollectionDetail.as_view(),
-         name='collection-detail'),
-    # path('', views.product_list, name='product_list'),
-    # path('tags/<slug:tag_slug>', views.product_list, name='product_list_by_tag'),
-    # path('<int:id>/<slug:slug>', views.product_detail, name='product_detail'),
+    path('', include(router.urls))
 ]
